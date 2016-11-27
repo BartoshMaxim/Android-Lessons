097512e40e1705a73f19d8e862e3d0cd273c104a
Ted KharkivTeam <bartosh.maxim@yandex.ua>
2016-11-27 21:07:35 +0200

    add show all data

---
+++ cookingbook/src/main/java/ru/startandroid/cookingbook/SearchActivity.java
@@ -0,0 +1,8 @@
+package ru.startandroid.cookingbook;
+
+/**
+ * Created by Ted KharkivTeam on 27-Nov-16.
+ */
+
+public class SearchActivity {
+}
--- cookingbook/src/main/java/ru/startandroid/cookingbook/ShowRecipleActivity.java
+++ cookingbook/src/main/java/ru/startandroid/cookingbook/ShowRecipleActivity.java
@@ -1,25 +1,36 @@
 package ru.startandroid.cookingbook;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class ShowRecipleActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ListView listRecipesShowRecipeActivity;
     List<Recipe> allRecipes;
+
+    ScrollView scrollViewShowRecipleActivity;
+    LinearLayout relativeLayoutShowRecipleActivity;
+    Button goBackShowRecipleActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipes);
 
-        listRecipesShowRecipeActivity = (ListView) findViewById(R.id.listRecipesShowRecipeActivity);
+        scrollViewShowRecipleActivity = (ScrollView) findViewById(R.id.scrollViewShowRecipleActivity);
+        relativeLayoutShowRecipleActivity = (LinearLayout) findViewById(R.id.relativeLayoutShowRecipleActivity);
+        goBackShowRecipleActivity = (Button) findViewById(R.id.goBackShowRecipleActivity);
+
+        goBackShowRecipleActivity.setOnClickListener(this);
 
         DatabaseRecipe dB = new DatabaseRecipe(this);
 
@@ -28,20 +39,46 @@ public class ShowRecipleActivity extends AppCompatActivity implements View.OnCli
 
         int i = 0;
         for (Recipe recipe : allRecipes) {
-            allNames[i] = recipe.get_name();
+            formRecipe(relativeLayoutShowRecipleActivity,recipe,i);
             i++;
         }
 
-// используем адаптер данных
-        ArrayAdapter<Recipe> adapter = new ArrayAdapter<Recipe>(this,
-                android.R.layout.simple_list_item_1, allRecipes);
-
-        listRecipesShowRecipeActivity.setAdapter(adapter);
-
     }
 
+
+    public void formRecipe(LinearLayout view, Recipe recipe,int id)
+    {
+        TextView nameView = new TextView(this);
+        TextView descriptionView = new TextView(this);
+        ImageView imageView = new ImageView(this);
+
+        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(30, 0, 0, 0);
+
+//        RelativeLayout.LayoutParams paramsDescription=new RelativeLayout.LayoutParams(
+//                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        paramsDescription.setMargins(30, 0, 0, 0);
+//
+//        RelativeLayout.LayoutParams paramsFoto=new RelativeLayout.LayoutParams(
+//                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        paramsFoto.setMargins(30, 0, 0, 0);
+
+        nameView.setText(recipe.get_name());
+        descriptionView.setText(recipe.get_desription());
+        imageView.setImageBitmap(recipe.get_fotoRecipe());
+
+        view.addView(nameView,params);
+        view.addView(descriptionView,params);
+        view.addView(imageView,params);
+
+    }
     @Override
     public void onClick(View view) {
-
+        if(view.getId() == R.id.goBackShowRecipleActivity){
+            Intent intentCancel = new Intent(this, MainActivity.class);
+            startActivity(intentCancel);
+            finish();
+        }
     }
 }
